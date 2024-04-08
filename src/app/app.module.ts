d40ef207867790754bd3c078b6a48633565b6072
NB1515\Miguel <migimartins96@gmail.com>
2024-04-08 22:43:52 +0200

    Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,33 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import locale from '@angular/common/locales/en-CH';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { registerLocaleData } from '@angular/common';
+import { AsyncPipe, registerLocaleData } from '@angular/common';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { environment } from '../environments/environment';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './shared/login/login.component';
+import { AuthInterceptor } from './shared/interceptor/auth.interceptor';
 
 registerLocaleData(locale);
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [AppRoutingModule, BrowserModule, HttpClientModule, IonicModule.forRoot()],
+  declarations: [AppComponent, LoginComponent],
+  imports: [
+    AppRoutingModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    BrowserModule,
+    HttpClientModule,
+    IonicModule.forRoot(),
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+  ],
   providers: [
     { provide: DEFAULT_CURRENCY_CODE, useValue: 'CHF' },
     { provide: LOCALE_ID, useValue: 'en-CH' },
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
   ],
-  bootstrap: [AppComponent],
 })
 export class AppModule {}
